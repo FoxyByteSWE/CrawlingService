5973b919b0a494574a8131921084e13d9a360467
Marco Uderzo <marco.uderzodj@gmail.com>
2022-06-23 17:35:13 +0200

    profilescraper base

---
+++ crawler/ProfileScraper.py
@@ -0,0 +1,22 @@
+import os, json, sys
+from instagrapi import Client
+import instagrapi
+from typing import Dict
+
+def createLoggedInClient():
+	client = Client()
+	client.login("foxybyte.swe", "Swe_2022")
+	return client
+
+
+#################################################################
+
+def main():
+    pass
+
+
+
+################################################################
+
+if __name__ == "__main__":
+    main()
