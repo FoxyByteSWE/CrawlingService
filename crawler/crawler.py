0e743b03a3241ae76cdc996de9e5ee5963ad756f
Marco Uderzo <marco.uderzodj@gmail.com>
2022-06-23 08:26:53 +0200

    relative path

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,5 +1,5 @@
 
-import os, json
+import os, json, sys
 from instagrapi import Client
 import instagrapi
 from typing import Dict
@@ -11,7 +11,7 @@ def createLoggedInClient():
 	return client
 
 def getAllCrawlableLocationsFromSomewhere():
-	txt_file = open("F:/OneDrive/Marco/UniPD/Triennale/Ingegneria del Software/Progetto/FoxyByte/IGCrawlerService/crawler/data/locations.txt", "r")
+	txt_file = open((str(sys.path[0]))+"\\data\\locations.txt", "r")
 	content_list = txt_file.readlines()
 	return content_list
 
@@ -27,8 +27,7 @@ def getLocationPkCodeFromName(locationName, client):
 
 def writeCrawledDataToJson(locationsData):  #TypeError: Object of type datetime is not JSON serializable
 	jsondump= json.dumps(locationsData)
-	os.chdir("F:/OneDrive/Marco/UniPD/Triennale/Ingegneria del Software/Progetto/FoxyByte/IGCrawlerService/crawler/data")
-	with open("locationsData.json", "a") as outfile:
+	with open((str(sys.path[0]))+"\\data\\locationsData.json", "a") as outfile:
 		outfile.write(jsondump)
 
 def crawlAllLocations(locationNamesList, client):
@@ -94,7 +93,7 @@ def getMediaURL(media):
 
 
 
-def formatMediaToDictionaryItem(media):
+def formatMediaToDictionaryItem(media): #need to serialize casting to primitive data types
 	formattedDictionaryMedia = {}
 	formattedDictionaryMedia["MediaType"] = getMediaType(media)
 	formattedDictionaryMedia["TakenAtTime"] = getMediaTime(media)
@@ -122,6 +121,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+#	{'Lunaelaltro\n': [{'MediaType': 1, 'TakenAtTime': datetime.datetime(2016, 4, 15, 18, 19, 44, tzinfo=datetime.timezone.utc), 'TakenAtLocationName': Location(pk=3110887, name='Ristorante Pizzeria Lunaelaltro - Marostica', phone='', website='', category='', hours={}, address=None, city=None, zip=None, lng=11.660707634193, lat=45.736862428411, external_id=77610911328, external_id_source='facebook_places'), 'LikeCount': 1, 'MediaURL': HttpUrl('https://scontent-mxp2-1.cdninstagram.com/v/t51.2885-15/11249882_966261376755731_963030927_n.jpg?se=8&stp=dst-jpg_e35&_nc_ht=scontent-mxp2-1.cdninstagram.com&_nc_cat=111&_nc_ohc=QeSTLR-83PoAX_ePcKi&edm=AKmAybEBAAAA&ccb=7-5&ig_cache_key=MTIyOTEzNTQ0NTAwMDU1ODY0Mg%3D%3D.2-ccb7-5&oh=00_AT90D8dLILH9q4LplVIJVV_F2eb_-rVfShWK7vi8fUZIBg&oe=62BA0E7D&_nc_sid=bcb968', scheme='https', host='scontent-mxp2-1.cdninstagram.com', tld='com', host_type='domain', port='443', path='/v/t51.2885-15/11249882_966261376755731_963030927_n.jpg', query='se=8&stp=dst-jpg_e35&_nc_ht=scontent-mxp2-1.cdninstagram.com&_nc_cat=111&_nc_ohc=QeSTLR-83PoAX_ePcKi&edm=AKmAybEBAAAA&ccb=7-5&ig_cache_key=MTIyOTEzNTQ0NTAwMDU1ODY0Mg%3D%3D.2-ccb7-5&oh=00_AT90D8dLILH9q4LplVIJVV_F2eb_-rVfShWK7vi8fUZIBg&oe=62BA0E7D&_nc_sid=bcb968')}
 
 
 
